857657d4a8ecbedcb6174a26beeacab88027922c
pharwell-code <p.harwell12@gmail.com>
2026-03-09 10:38:14 -0700

    Update Magmar.py

--- TVPoke/Pokemon/Magmar.py
+++ TVPoke/Pokemon/Magmar.py
@@ -9,4 +9,4 @@ class Magmar(Fire):
             Move("Sacred Fire", "FIRE", 1000),
             Move("Eruption", "FIRE", 2000)
         ]
-        super().__init__("Magmar", 50000, moves, "magmar.png")
+        super().__init__("Magmar", 50000, moves, "Magmar.png")
